e39313c3431807beab26edb95a2f4ba7f0804b93
devsince2021 <dev.ryu2021@gmail.com>
2021-05-01 21:58:51 +0900

    change master to main

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('it worked');
+});
+
+io.on('connection', socket => {
+  console.log('A user connected :D');
+
+  socket.on('send-location', (geolocation) => {
+
+    console.log('geolocation', geolocation);
+    socket.emit('receive-location', geolocation);
+    console.log('delevered');
+  })
+})
+
+server.listen(port, () => console.log('server is running on port: ' + port));
